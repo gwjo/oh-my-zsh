096d9ec4ec20f463c2e7f648f3322ec3f5c1dbfe
gwjo <gowen72@gmail.com>
2010-10-06 08:55:24 -0400

    Don't complete backup files when looking for executables

--- custom/completions.zsh
+++ custom/completions.zsh
@@ -1,12 +1,20 @@
-# Split matches into groups
+# {{{ Split matches into groups
 zstyle ':completion:*:matches' group 'yes'
 
-# Group descriptions/message/warning formats
+# }}}
+# {{{ Group descriptions/message/warning formats
 zstyle ':completion:*:descriptions' format "%B---- %d%b"
 zstyle ':completion:*:messages' format '%B%U---- %d%u%b' 
 zstyle ':completion:*:warnings' format '%B%U---- no match for: %d%u%b'
 
-# Describe options in full
+# }}}
+# {{{ Describe options in full
 zstyle ':completion:*:options' description 'yes'
 zstyle ':completion:*:options' auto-description '%d'
+
+# }}}
+# {{{ Don't complete backup files (e.g. 'bin/foo~') as executables
+zstyle ':completion:*:complete:-command-::commands' ignored-patterns '*\~'
+
+# }}}
 
