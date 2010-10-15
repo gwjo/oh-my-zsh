4c98f8e15a509c17a593a4094d4aa730d7b38557
gwjo <gowen72@gmail.com>
2010-10-15 17:45:55 -0400

    Work related stuff

---
+++ custom/work.zsh
@@ -0,0 +1,21 @@
+## Some development related stuff
+
+## {{{ update tags and cscopte
+
+
+function uptags() {
+
+    local ccviewtop=`ccbase`
+
+    if [[ -z $ccviewtop ]] ; then
+        echo "Not in a clearcase view!"
+        return
+    fi
+
+    print -rC1 $ccviewtop/**/*.[ch](|pp) > cscope.files
+
+    #/usr/bin/cscope -b -i $ccviewtop/cscope.files -f $ccviewtop/cscope.out
+    /usr/bin/ctags -L $ccviewtop/cscope.files --extra=+q -o $ccviewtop/tags
+}
+
+## }}}
