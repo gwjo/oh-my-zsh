981b9c6b0661881e1188079df1498b6e7601b262
gwjo <gowen72@gmail.com>
2010-11-04 14:23:02 -0400

    Fix tags function

--- custom/work.zsh
+++ custom/work.zsh
@@ -7,14 +7,14 @@ function uptags() {
 
     local ccviewtop=`ccbase`
 
-    if [[ -z $ccviewtop ]] ; then
+    if [[ -z ${ccviewtop} ]] ; then
         echo "Not in a clearcase view!"
         return
     fi
 
-    print -rC1 $ccviewtop/**/*.[ch](|pp) > cscope.files
+    print -rC1 ${ccviewtop}/**/*.[ch](|pp) > ${ccviewtop}/cscope.files
 
-    #/usr/bin/cscope -b -i $ccviewtop/cscope.files -f $ccviewtop/cscope.out
+    /usr/bin/cscope -b -i $ccviewtop/cscope.files -f $ccviewtop/cscope.out
     /usr/bin/ctags -L $ccviewtop/cscope.files --extra=+q -o $ccviewtop/tags
 }
 
