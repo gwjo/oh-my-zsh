348852d5d2a8d92d3a209dff4f1f3e7ab81aa859
gwjo <gowen72@gmail.com>
2010-11-08 12:26:27 -0500

    Aliases and functions for moving around code base

--- custom/work.zsh
+++ custom/work.zsh
@@ -19,3 +19,63 @@ function uptags() {
 }
 
 ## }}}
+
+## {{{ change to directory based on SD stream
+
+#
+# Usage:
+#
+#  sd_cd <SD2 dir> <SD4 dir>
+#
+function sd_cd() {
+
+    local cc_base=`/usr/local/acme/bin/ccbase`
+
+    if [[ -z ${cc_base} ]] ; then
+        echo "Not in a clearcase view"
+        return
+    fi
+
+    if [[ -d ${cc_base}/$1 ]] ; then
+        cd ${cc_base}/$1
+    else
+        cd ${cc_base}/$2
+    fi
+}
+
+## }}}
+## {{{ change to a acme directory
+
+function acme_cd() {
+    sd_cd acme/$1 sd_iv/acme/$2
+}
+
+## }}}
+## {{{ change to an app directory
+function app_cd() {
+    acme_cd bin/$1 apps/$1
+}
+
+## }}}
+## {{{ alias to move around the code base
+
+alias base='sd_cd . .'
+alias acme='acme_cd . .'
+alias apps='app_cd .'
+alias bin='app_cd .'
+alias inc='acme_cd include/acme include'
+
+alias acli='app_cd acli'
+alias algd='app_cd algd'   # sd2 only
+alias atcp='app_cd atcp'
+alias h248='app_cd h248'
+alias h323='app_cd h323'
+alias mbcd='app_cd mbcd'
+alias mgcp='app_cd algd'   # use this instead of algd (sd2 only)
+alias sip= 'app_cd sip'
+
+alias account='acme_cd lib/accounting lib/accounting'
+alias common='acme_cd lib/common lib/common'
+alias dam='acme_cd lib/dam lib/dam'
+
+## }}}
